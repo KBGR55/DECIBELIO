382820d4f44563d8bf9a679e529012cd67d26f16
KBGR55 <brigithgonzaga501@gmail.com>
2025-06-18 10:17:19 -0500

    Update

--- decibelio_app_web/build/web/flutter_service_worker.js
+++ decibelio_app_web/build/web/flutter_service_worker.js
@@ -63,11 +63,11 @@ const RESOURCES = {"favicon.png": "3c0404079db0c570f6092a0209017140",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
-"main.dart.js": "6dff563cdef739535ec374f244ec800c",
+"main.dart.js": "894e82d54ad29f26573032857aaa2786",
 "index.html": "e9b2e2f15c465d350ad093ca42da8c97",
 "/": "e9b2e2f15c465d350ad093ca42da8c97",
 "version.json": "82cf3c412ed646eb6828076507a64a0c",
-"flutter_bootstrap.js": "ab8cf44cad9661b2b4990c8b4a9227d0",
+"flutter_bootstrap.js": "8f5ad3ac3a92e34a073460fb3dadf63d",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea"};
 // The application shell files that are downloaded before a service worker can
 // start.
